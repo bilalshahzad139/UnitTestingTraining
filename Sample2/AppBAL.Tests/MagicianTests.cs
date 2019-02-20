5e1cc904f184096878b6ab3b24075ce7eb6f9b1b
Bilal Shahzad (66110) <bilal.shahzad@ibexglobal.com>
2019-02-21 01:40:11 +0500

    Sample with NUnit

---
+++ Sample2/AppBAL.Tests/MagicianTests.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBAL.Tests
+{
+    [TestFixture]
+    public class MagicianTests
+    {
+        [Test]
+        public void DoMagic_Testing1()
+        {
+            //Arrange
+            int expectedResult = 10;
+            //Act
+            Magician magObj = new Magician();
+            int result = magObj.DoMagic("ABC");
+
+            //Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+        [Test]
+        public void DoMagic_Testing2()
+        {
+            //Arrange
+            int expectedResult = 20;
+            //Act
+            Magician magObj = new Magician();
+            int result = magObj.DoMagic("XYZ");
+
+            //Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+        [Test]
+        public void DoMagic_Testing3()
+        {
+            //Arrange
+            int expectedResult = 30;
+            //Act
+            Magician magObj = new Magician();
+            int result = magObj.DoMagic("");
+
+            //Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+
+        [TestCase("ABC",10)]
+        [TestCase("XYZ", 20)]
+        [TestCase("X", 30)]
+        [TestCase("", 30)]
+        public void DoMagic_Testing(String inputValue, int expectedResult)
+        {
+            //Arrange
+            //int expectedResult = 10;
+            //Act
+            Magician magObj = new Magician();
+            int result = magObj.DoMagic(inputValue);
+
+            //Assert
+            Assert.AreEqual(result, expectedResult);
+        }
+    }
+}
---
+++ Sample2/AppBAL/Magician.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppBAL
+{
+    public class Magician
+    {
+        public int DoMagic(String param)
+        {
+            if (param.Equals("ABC"))
+                return 10;
+            else if (param.Equals("XYZ"))
+                return 20;
+            else
+                return 30;
+        }
+
+        //public int CalculateMagicCost(int pMagicTricks)
+        //{
+        //    AppHelper.MagicHelper magicObj = new AppHelper.MagicHelper();
+        //    if (magicObj.GetMagicComplexity(pMagicTricks) > 10)
+        //        return 100;
+        //    else
+        //        return 50;
+        //}
+    }
+}
