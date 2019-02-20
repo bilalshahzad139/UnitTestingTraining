3506b39575a3021ec41ce3bfd05367bfcc7892ac
Bilal Shahzad (66110) <bilal.shahzad@ibexglobal.com>
2019-02-21 02:38:35 +0500

    With NUnit - External Dependency in Unit

---
+++ Sample3/AppHelper/MagicHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppHelper
+{
+    public class MagicHelper
+    {
+        public int GetMagicComplexity(int pMagicTricks)
+        {
+            return pMagicTricks * 3 + 1;
+        }
+    }
+}
