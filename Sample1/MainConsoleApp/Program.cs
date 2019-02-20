80b7efe57abb3de5408853b0552faa7a98965ff6
Bilal Shahzad (66110) <bilal.shahzad@ibexglobal.com>
2019-02-21 00:48:06 +0500

    Sample 1 (without NUnit)

---
+++ Sample1/MainConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppBAL.Magician magObj = new AppBAL.Magician();
+            int result = magObj.DoMagic("ABC");
+
+            if (result == 10)
+                System.Console.WriteLine("Test Passed");
+
+            System.Console.ReadKey();
+        }
+    }
+}
